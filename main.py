5069f16467de9bd54ee586aebd93ed3f17618977
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2020-11-11 13:57:46 +0300

    connection and account inf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import MetaTrader5 as mt5
 from config import login_info
+import pandas as pd
 
 # initialize server variables (login details)
 path=login_info["path"]
@@ -15,11 +16,26 @@ print("MetaTrader5 package version: ",mt5.__version__)
 if not mt5.initialize(path,login=login,server=server,password=password):
     print("initialize() failed, error code =",mt5.last_error())
     quit()
- 
-# display data on connection status, server name and trading account
-print(mt5.terminal_info()) 
+
 # display data on MetaTrader 5 version
 print(mt5.version())
+# display data on connection status, server name and trading account
+terminal_info=mt5.terminal_info()
+
  
+if terminal_info!=None:
+    # display the terminal data 'as is'
+    print(terminal_info)
+    # display data in the form of a list
+    print("Show terminal_info()._asdict():")
+    terminal_info_dict = mt5.terminal_info()._asdict()
+    for prop in terminal_info_dict:
+        print("  {}={}".format(prop, terminal_info_dict[prop]))
+    print()
+    # convert the dictionary into DataFrame and print
+    df=pd.DataFrame(list(terminal_info_dict.items()),columns=['property','value'])
+    print("terminal_info() as dataframe:")
+    print(df)
+
 # shut down connection to the MetaTrader 5 terminal
-mt5.shutdown()
+mt5.shutdown() 
